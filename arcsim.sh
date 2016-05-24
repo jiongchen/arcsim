2a39ca257316bc98436aa0c1a47abbcabfcbdc24
JiongCHEN <gakki91@gmail.com>
2016-05-24 13:40:19 +0800

    add flag4

--- arcsim.sh
+++ arcsim.sh
@@ -2,12 +2,15 @@
 
 exe=build/src/arcsim
 #cf=conf/flag.json
-cf=conf/sphere_copy.json
+#cf=conf/sphere_copy.json
+cf=conf/dress-blue.json
 
 #rm ./build/march29/*
 #$exe simulate $cf 
 #$exe simulateoffline $cf ./build/windy_flag
-$exe simulateoffline $cf ./build/ball
+
+#$exe simulateoffline $cf ./build/ball
+$exe simulate $cf
 
 #$exe simulate conf/dress-blue.json
 #$exe resumeoffline ./build/march29
